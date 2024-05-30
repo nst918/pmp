c902c72386e1572e6e049f5d3454d9fb49fcc234
manvi.arora <manvi.arora@svam.com>
2024-05-30 17:27:46 +0530

    login removed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,23 +5,23 @@ import { AppRouteGuard } from './core/auth/auth-route-guard';
 export const routes: Routes = [
   { 
     path: 'my-attendance',
-    canActivate: [AppRouteGuard],
+    // canActivate: [AppRouteGuard],
     loadChildren: () => import('./my-attendance/my-attendance.module').then(m => m.MyAttendanceModule) },
   { 
     path: 'projects',
-    canActivate: [AppRouteGuard],
+    // canActivate: [AppRouteGuard],
     loadChildren: () => import('./my-project/my-project.module').then(m => m.MyProjectModule) },
   { 
     path: 'report',
-    canActivate: [AppRouteGuard],
+    // canActivate: [AppRouteGuard],
     loadChildren: () => import('./report/report.module').then(m => m.ReportModule) },
   { 
     path: 'requests',
-    canActivate: [AppRouteGuard],
+    // canActivate: [AppRouteGuard],
     loadChildren: () => import('./request/request.module').then(m => m.RequestModule) },
   { 
     path: 'resource-sheet',
-    canActivate: [AppRouteGuard],
+    // canActivate: [AppRouteGuard],
     loadChildren: () => import('./resource-sheet/resource-sheet.module').then(m => m.ResourceSheetModule) },
   { path: '**', redirectTo: '' },
 ];
